f8af852fd68d060d21803ab22143da12357c6bb7
Malolan Balaji <mbalaji@atlassian.com>
2025-02-10 11:30:35 +0530

    remove lightningcss

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	experimental: {
 		cssChunking: true,
-		useLightningcss: true,
 		webVitalsAttribution: ['CLS', 'LCP'],
 	},
 };
