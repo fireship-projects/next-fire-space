499e1fb9b16605ff895abb0e56be12fd299c9f8c
Malolan Balaji <mbalaji@atlassian.com>
2025-02-16 12:42:45 +0530

    add loading and error components for user data, implement user loading state, and update user display with avatar and link

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,5 @@
-import { UserCard } from '@/components/user-components/user-card';
 import { prisma } from '@/modules/db/prisma';
+import Link from 'next/link';
 
 export default async function Users() {
 	const users = await prisma.user.findMany();
@@ -7,12 +7,21 @@ export default async function Users() {
 	return (
 		<div className="stats stats-horizontal p-4">
 			{users.map((user) => (
-				<UserCard
-					key={user.id}
-					email={user.email ?? ''}
-					name={user.name ?? ''}
-					avatarUrl={user.image ?? ''}
-				/>
+				<div key={user.id} className="stat border rounded-sm">
+					<div className="stat-figure text-secondary">
+						{user.image ? (
+							<div className="avatar online">
+								<div className="w-16 rounded-full">
+									<img src={user.image} alt="avatar of a person" />
+								</div>
+							</div>
+						) : null}
+					</div>
+					<Link className="link link-hover" href={`/users/${user.id}`}>
+						{user.name}
+					</Link>
+					<div className="stat-desc text-secondary">{user.email}</div>
+				</div>
 			))}
 		</div>
 	);
